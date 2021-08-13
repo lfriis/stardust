47ceb111f9c13f0e550d8e8eacbb89c310f15967
lfriis <larsenfriis@icloud.com>
2021-08-12 23:28:50 -0400

    Camera now setting to users location on load

--- client/src/components/map/Map.tsx
+++ client/src/components/map/Map.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useRef, useEffect } from 'react';
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { CoordinateProps } from '../../interfaces';
 import mapStyles from '../../styles/mapStyles';
 
 export default function Map(props: CoordinateProps): JSX.Element {
+	// const [region, setRegion] = useState<object>({});
+	const _map = useRef(null);
+	const _camera = {
+		center: {
+			latitude: props.latitude,
+			longitude: props.longitude,
+		},
+		pitch: 0,
+		heading: 0,
+		zoom: 15,
+	};
+
+	useEffect(() => {
+		_map.current.setCamera(_camera);
+		_map.current.getCamera(props).then((res) => console.log(res));
+	}, [props]);
+
 	return (
 		<>
 			<MapView
+				ref={_map}
 				provider={PROVIDER_GOOGLE}
 				region={{
 					latitude: props.latitude,
@@ -16,15 +34,18 @@ export default function Map(props: CoordinateProps): JSX.Element {
 				}}
 				style={mapStyles.base}
 				showsUserLocation
-			/>
+				showsMyLocationButton
+				followsUserLocation
 
-			<Marker
-				coordinate={{
-					latitude: 38.5815719,
-					longitude: -121.4943996,
-				}}
-				image={require('../../assets/images/res.png')}
-			/>
+				// onRegionChangeComplete={(region) => setRegion(region)}
+			>
+				<Marker
+					coordinate={{
+						latitude: 43.65373,
+						longitude: -79.36194,
+					}}
+				/>
+			</MapView>
 		</>
 	);
 }
